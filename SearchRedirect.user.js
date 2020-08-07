ead7e3efd5d81683940451f9f1959329e5ddb6ac
Keith <10713450+kewalsky@users.noreply.github.com>
2020-08-07 14:29:46 -0400

    Update SearchRedirect.user.js

--- SearchRedirect.user.js
+++ SearchRedirect.user.js
@@ -1,9 +1,7 @@
 // ==UserScript==
 // @name SearchRedirect
 // @namespace github.com/kewalsky/userscripts
-// @version 2020-08-07
-// @author Various
-// @description greasyfork.org/en/scripts/11308-bing-to-google
+// @version 2020-08-07a
 // @include http://*.bing.com/search?*
 // @include https://*.bing.com/search?*
 // @updateURL https://raw.githubusercontent.com/kewalsky/userscripts/master/SearchRedirect.user.js
@@ -11,6 +9,5 @@
 // @run-at document-start
 // @grant none
 // ==/UserScript==
-
 var SearchURL = "https://google.com/search?"+document.URL.match(/q\=[^&]*/);
 if (SearchURL != document.URL) location.replace(SearchURL);
